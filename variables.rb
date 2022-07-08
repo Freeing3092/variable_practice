4a5fa8568d7a8248b002fbb3f4f6a4a018681046
Rich Kaht <adamselene1776@protonmail.com>
2022-07-08 15:44:42 -0600

    Amend strings and add puts

--- variables.rb
+++ variables.rb
@@ -9,3 +9,11 @@ float_two = 2.2
 
 enrolled_in_BE = true
 completed_mod0 = false
+
+name = "Rich"
+school = "Turing School of Software and Design"
+
+puts("Hello, my name is ")
+puts(name)
+puts("I am currently enrolled in ")
+puts(school)
